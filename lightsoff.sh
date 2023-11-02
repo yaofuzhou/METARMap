1804441a02604a1afeaaf4a2c597bde688bc60f8
yaofuzhou <yaofuzhou@users.noreply.github.com>
2023-11-02 00:17:39 -0400

    slight change in how to turn it off

--- lightsoff.sh
+++ lightsoff.sh
@@ -1,3 +1,6 @@
 /usr/bin/sudo pkill -F /home/pi/METARMap/offpid.pid
 /usr/bin/sudo pkill -F /home/pi/METARMap/metarpid.pid
 /usr/bin/sudo /usr/bin/python3 /home/pi/METARMap/pixelsoff.py & echo $! > /home/pi/METARMap/offpid.pid
+
+# Create a stop_refresh file to signal the refresh loop to stop
+touch stop_refresh
--- off.sh
+++ off.sh
@@ -6,6 +6,4 @@
 # Update the suntimes.csv for the next use
 echo "Updating suntimes.csv for the next use..."
 sudo python3 suntimes.py
-
-# Create a stop_refresh file to signal the refresh loop to stop
-touch stop_refresh
+echo "Good to go tomorrow!"
