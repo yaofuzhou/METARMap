f0d88e58c685b2b7cd7f331995f35090f7b61cda
yaofuzhou <yaofuzhou@users.noreply.github.com>
2023-11-02 00:01:46 -0400

    on and off updated

--- on.sh
+++ on.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# Function to wait for internet connection
+wait_for_internet() {
+  local test_ip
+  test_ip=8.8.8.8
+
+  # Wait for internet up to 5 minutes
+  local timeout=300
+
+  while [ $timeout -gt 0 ]; do
+    if ping -c 1 $test_ip &> /dev/null; then
+      echo "Internet is up"
+      return 0
+    else
+      echo "Waiting for internet connection..."
+      sleep 10
+      timeout=$((timeout-10))
+    fi
+  done
+
+  echo "Timed out waiting for internet connection. Please check your network settings."
+  return 1
+}
+
+# Call the function to wait for internet connection
+wait_for_internet || exit 1
+
 # Path to the suntimes.csv file
 SUNTIMES_CSV="suntimes.csv"
 
@@ -15,5 +41,4 @@ rm -f stop_refresh
 # Run the refresh script in a loop
 while [ ! -f stop_refresh ]; do
   ./refresh.sh
-  sleep 300
 done
