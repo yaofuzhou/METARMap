cd1effde9b1e9b244ee48491a4fd5971346bcc5d
yaofuzhou <yaofuzhou@users.noreply.github.com>
2023-11-02 00:50:27 -0400

    on

--- on.sh
+++ on.sh
@@ -41,4 +41,5 @@ rm -f stop_refresh
 # Run the refresh script in a loop
 while [ ! -f stop_refresh ]; do
   ./refresh.sh
+  sleep 300
 done
--- refresh.sh
+++ refresh.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-# Main loop
-while true; do
-  # Check if the stop_refresh file exists. If it does, exit the loop.
-  if [ -f stop_refresh ]; then
-    echo "Found stop_refresh file. Exiting."
-    break
-  fi
-
-  /usr/bin/sudo pkill -F /home/pi/METARMap/offpid.pid
-  /usr/bin/sudo pkill -F /home/pi/METARMap/metarpid.pid
-  /usr/bin/sudo /usr/bin/python3 /home/pi/METARMap/metar.py & echo $! > /home/pi/METARMap/metarpid.pid
-
-  # Sleep for 300 seconds (5 minutes) before running again
-  sleep 300
-done
+/usr/bin/sudo pkill -F /home/pi/METARMap/offpid.pid
+/usr/bin/sudo pkill -F /home/pi/METARMap/metarpid.pid
+/usr/bin/sudo /usr/bin/python3 /home/pi/METARMap/metar.py & echo $! > /home/pi/METARMap/metarpid.pid
