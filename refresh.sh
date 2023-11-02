9046d01e3674d7330b77fa849fe9d66653c4666f
yaofuzhou <yaofuzhou@users.noreply.github.com>
2023-11-02 00:46:25 -0400

    space

--- refresh.sh
+++ refresh.sh
@@ -12,7 +12,6 @@ while true; do
   /usr/bin/sudo pkill -F /home/pi/METARMap/metarpid.pid
   /usr/bin/sudo /usr/bin/python3 /home/pi/METARMap/metar.py & echo $! > /home/pi/METARMap/metarpid.pid
 
-
   # Sleep for 300 seconds (5 minutes) before running again
   sleep 300
 done
