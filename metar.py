bfcdaa7a2e680c4fb9deaa95c9a86589d4f71034
yaofuzhou <yaofuzhou@users.noreply.github.com>
2023-11-12 18:45:11 -0500

    settings back to my own needs

--- metar.py
+++ metar.py
@@ -50,8 +50,8 @@ ACTIVATE_LIGHTNING_ANIMATION     = True             # Set this to False for Stat
 # Fade instead of blink
 FADE_INSTEAD_OF_BLINK            = True             # Set to False if you want blinking
 # Blinking Windspeed Threshold
-WIND_BLINK_THRESHOLD             = 50               # Knots of windspeed to blink/fade
-HIGH_WINDS_THRESHOLD             = 75               # Knots of windspeed to trigger Yellow LED indicating very High Winds, set to -1 if you don't want to use this
+WIND_BLINK_THRESHOLD             = 15               # Knots of windspeed to blink/fade
+HIGH_WINDS_THRESHOLD             = 25               # Knots of windspeed to trigger Yellow LED indicating very High Winds, set to -1 if you don't want to use this
 ALWAYS_BLINK_FOR_GUSTS           = True             # Always animate for Gusts (regardless of speeds)
 # Blinking Speed in seconds
 BLINK_SPEED                      = 0.5              # Float in seconds, e.g. 0.5 for half a second
