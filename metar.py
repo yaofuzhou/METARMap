faf65db65ef6fc417ababb2f4f7e4f96abc38031
yaofuzhou <yaofuzhou@users.noreply.github.com>
2023-12-12 02:32:26 -0500

    test only

--- metar.py
+++ metar.py
@@ -416,8 +416,8 @@ while looplimit > 0:
         try:
             iss_x = float(iss_position['latitude'])
             iss_y = float(iss_position['longitude'])
-            # light_up_iss_rings(iss_x, iss_y, airports_data, pixels, current_led_colors, 0.85)
-            light_up_iss_rings(-80.3944, 36.66505, airports_data, pixels, current_led_colors, 0.85)  # for test
+            light_up_iss_rings(iss_x, iss_y, airports_data, pixels, current_led_colors, 0.85)
+            # light_up_iss_rings(-80.3944, 36.66505, airports_data, pixels, current_led_colors, 0.85)  # for test
         except Exception as e:
             print(f"Error in ISS animation: {e}")
 
