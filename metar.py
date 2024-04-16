bb36a46fb9a19be84ec78af9a25b8bc69327d53b
yaofuzhou <yaofuzhou@users.noreply.github.com>
2024-04-16 03:00:05 -0400

    more robust TS

--- metar.py
+++ metar.py
@@ -404,7 +404,14 @@ while looplimit > 0:
             else:
                 color = COLOR_CLEAR
 
-        print("Setting LED " + str(i) + " for " + airportcode + " to " + ("lightning " if lightningConditions else "") + ("very " if highWinds else "") + ("windy " if windy else "") + (conditions["flightCategory"] if conditions != None else "None") + " " + str(color))
+        print("Setting LED " + str(i) + " for " + (airportcode if airportcode is not None else "Unknown") + " to " + 
+      ("lightning " if lightningConditions else "") + 
+      ("very " if highWinds else "") + 
+      ("windy " if windy else "") + 
+      (conditions["flightCategory"] if conditions is not None and "flightCategory" in conditions else "None") + 
+      " " + 
+      (str(color) if color is not None else "No Color"))
+
 
         print("brightness_adjustment =", brightness_adjustment)
 
